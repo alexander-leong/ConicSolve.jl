0c696381aa18df383135e5feae4f69c7e2ecac01
Alexander Leong <toshiba_alexander@live.com>
2025-12-05 22:46:40 +1000

    WIP, refactor working

--- src/models/sos_sg.jl
+++ src/models/sos_sg.jl
@@ -9,12 +9,31 @@ using AbstractPermutations
 using DynamicPolynomials
 using GroupsCore
 using LinearAlgebra
+using MultivariateBases
 using PermutationGroups
 using StarAlgebras
 using SparseArrays
 using SymbolicWedderburn
 
 mutable struct SOS_Symmetric_Group
+    basis
+    basis_half
+    basis_type
+    deg
+    equality_constraint_indices
+    f
+    SOS_Symmetric_Group() = new()
+    SOS_Symmetric_Group(basis,
+                        basis_half,
+                        basis_type,
+                        deg,
+                        equality_constraint_indices,
+                        f) = new(basis,
+                            basis_half,
+                            basis_type,
+                            deg,
+                            equality_constraint_indices,
+                            f)
 end
 
 function SymbolicWedderburn.action(a::SymbolicWedderburn.Action, el::GroupElement, poly::DynamicPolynomials.AbstractPolynomial)
@@ -71,52 +90,95 @@ function get_lt_vals(A)
     return lt_vals
 end
 
-U_map = Dict()
-
-function reduce_psd(A, i)
-    A, U, S = tsvd(A)
-    U_map[i] = (U, S)
-    return diagm(S)
+function get_mat_from_lt_vec(v, N)
+    V = zeros((N, N))
+    i = 1
+    for n in 1:N
+        for m in n:N
+            V[m, n] = v[i]
+            V[n, m] = v[i]
+            i += 1
+        end
+    end
+    return V
 end
 
-function set_A_i!(A, Mπs)
-	num_vars = size(A, 2)
+function get_vec_from_lt_mat(A)
+    v = []
+    N = size(A, 1)
+    for n in 1:N
+        for m in n:N
+            push!(v, A[m, n])
+        end
+    end
+    return v
+end
+
+function get_mat_dim(v)
+    i = 1
+    while i < length(v)
+        if (i * (i + 1)) / 2 == length(v)
+            return i
+        end
+        i += 1
+    end
+    return 1
+end
+
+function set_A_i!(A, Mπs, equality_constraint_indices)
     j = 0
     m = 0
-	# println(length(axes(Mπs, 1)))
     for Mπs_j in Mπs
         idx = j
 		idx += 1
         j += Int(size(Mπs_j, 1) * (size(Mπs_j, 1) + 1) / 2)
-        # get vectorized lower triangular entries of Mπs[i]
-        Mπs_j, U, S = tsvd(Mπs_j)
-        # eigmax used for computing regularized term cannot handle complex eigenvalues
-        Mπs_j = regularize(Mπs_j)
-        # cn = cond(Mπs_j)
-        # if cn > 1e2
-        S = svd(Mπs_j).S
-        σ_max, σ_min = S[1], S[end]
-        # println("WARNING: Poorly conditioned PSD condition generated! (condition number = $(cn))")
-        println("PSD condition generated (n, σ_max, σ_min = $(length(S)), $(σ_max), $(σ_min))")
-        if length(S) == 3 && σ_max > 0 && isapprox(σ_min, 0)
-            println(Mπs_j)
-        end
-        # end
-		Mπ_vec = get_lt_vals(Mπs_j)
 		
-        if iszero(Mπ_vec)
+        if iszero(Mπs_j)
+            # println("Omitting generated PSD constraint")
             continue
         end
-        a = zeros((1, num_vars))
-        a[idx:j] .= Mπ_vec
+        # TODO: remove regularization logic, refactor into face reduction module
+        # eigmax used for computing regularized term cannot handle complex eigenvalues
+        # regularized_Mπs_j, S = regularize(Mπs_j)
+        # S = svd(regularized_Mπs_j).S
+        # σ_max, σ_min = S[1], S[end]
+        # num_redundant = count(s -> s < 1e-6, S)
+        # if σ_min < 1e-3
+            # println(regularized_Mπs_j)
+            # println("PSD condition generated (n, num_redundant, σ_max, σ_min = $(length(S)), $(num_redundant), $(σ_max), $(σ_min))")
+        # end
+        # get vectorized lower triangular entries of Mπs[i]
+        Mπ_vec = get_lt_vals(Mπs_j)
+
+        # S = svd(Mπs_j).S
+        # σ_max, σ_min = S[1], S[end]
+        # num_redundant = count(s -> s < 1e-6, S)
+        # println("PSD condition generated (n, num_redundant, σ_max, σ_min = $(length(S)), $(num_redundant), $(σ_max), $(σ_min))")
+        a = get_constraint(A, Mπ_vec, idx)
         A = vcat(A, a)
+        push!(equality_constraint_indices, (idx, j))
+        # println("$(idx), $(length(idx:j))")
         m += 1
     end
     return A, m
 end
 
+function get_num_vars(Mπs_j, j)
+    j = Int(size(Mπs_j, 1) * (size(Mπs_j, 1) + 1) / 2)
+    return j
+end
+
+function get_constraint(A, Mπ_vec, idx)
+    num_vars = size(A, 2)
+    a = zeros((1, num_vars))
+    j = idx + length(Mπ_vec) - 1
+    # set values
+    a[idx:j] .= Mπ_vec
+    return a
+end
+
 # rank revealing qr, remove redundant constraints
-function rrqr(A, b, tol=1e-3)
+function rrqr(A, b, tol=1e-9)
     F = qr(A', ColumnNorm())
     # get list of row indices where diagonal elements of R satisfy tol
     inds = [i for (i, v) in enumerate(diag(F.R)) if abs(v) >= tol]
@@ -124,34 +186,30 @@ function rrqr(A, b, tol=1e-3)
     # reduced A, b
     rA = A[inds, :]
     rb = b[inds]
-    return rA, rb
+    return rA, rb, inds
 end
 
-function tsvd(A, tol=1e-9)
-    F = svd(A)
-    S::Vector{Float64} = []
-    for v in F.S
-        if v >= tol
-            push!(S, v)
-        end
-    end
-    U = F.U[:, 1:length(S)]
-    Vt = F.Vt[1:length(S), :]
-    A = U * diagm(S) * Vt
-    return Hermitian(A), U, S
-end
-
-function regularize(A, tol=1e-9)
-    eps = tol * eigmax(A)
-    A = A + eps * I
-    return A
-end
-
-function decompose(f, n, x, num_additional_vars=0)
-    deg = DynamicPolynomials.maxdegree(f)
+function get_monomial_basis(f, deg)
 	vars = DynamicPolynomials.variables(f)
 	basis = DynamicPolynomials.monomials(vars, 0:deg) # basis_constraints
     basis_half = DynamicPolynomials.monomials(vars, 0:deg÷2) # basis_psd
+    return basis, basis_half
+end
+
+# TODO: remove unsupported logic
+# NOTE: SymbolicWedderburn.jl does not currently support other basis types
+function get_chebyshev_basis(f, deg)
+    basis, basis_half = get_monomial_basis(f, deg)
+    basis = basis_covering_monomials(ChebyshevBasis, basis)
+    basis_half = basis_covering_monomials(ChebyshevBasis, basis_half)
+    return basis.polynomials, basis_half.polynomials
+end
+
+function wedderburn_decompose(f, n, x, num_additional_vars=0)
+    deg = DynamicPolynomials.maxdegree(f)
+    println("Polynomial function f has degree: $(deg)")
+    println("Symmetric Group $(n)")
+    basis, basis_half = get_monomial_basis(f, deg)
 
 	pg = PermGroup([perm"(1,2)", Perm([2:n; 1])])
 	wedderburn = WedderburnDecomposition(
@@ -171,6 +229,7 @@ function decompose(f, n, x, num_additional_vars=0)
     num_vars = Int(sum([size(psd, 1) * (size(psd, 1) + 1) / 2 for psd in psds]))
 
     # FIXME: relocate this logic outside
+    U_map = Dict()
     # Matrix A defining equality constraints in Ax = b
     total_num_vars = num_vars + num_additional_vars
     A = Matrix{Float64}(undef, 0, Int(total_num_vars))
@@ -181,32 +240,48 @@ function decompose(f, n, x, num_additional_vars=0)
     C = DynamicPolynomials.coefficients(f, basis_constraints)
     
     ivs = SymbolicWedderburn.invariant_vectors(wedderburn)
+    equality_constraint_indices = []
     for iv in ivs
         c = dot(C, iv)
 	    M_orb_ivc = invariant_constraint!(M_orb, M, iv)
         Mπs = SymbolicWedderburn.diagonalize(M_orb_ivc, wedderburn)
-        A, m = set_A_i!(A, Mπs)
+        A, m = set_A_i!(A, Mπs, equality_constraint_indices)
         b = append!(b, fill(c, m))
+        # println("End of Invariant Vector")
     end
+    println("Length of A: $(size(A, 1))")
+    println("Length of Equality Inds: $(length(equality_constraint_indices))")
 
-    A, b = rrqr(A, b)
+    A, b, inds = rrqr(A, b)
+    equality_constraint_indices = equality_constraint_indices[inds]
     cn = cond(A)
     if cn > 1e2
-        println("WARNING: Poorly conditioned equality matrix defined! (condition number = $(cn)")
+        println("WARNING: Poorly conditioned equality matrix defined! (condition number = $(cn))")
     end
-    return A, b, num_vars, psds
+
+    sos_symmetric_group = SOS_Symmetric_Group(
+        basis,
+        basis_half,
+        "monomial",
+        deg,
+        equality_constraint_indices,
+        f
+    )
+    println("Num rows in A: $(size(A, 1))")
+    println("End of decompose, returning...")
+    return A, b, num_vars, psds, sos_symmetric_group
 end
 
 function get_value_in_original_basis(psds, x)
     # get block diagonal form from solution vector x
     n_i = [size(X, 1) for X in psds_mat]
     inds = [1, cumsum(n_i)...]
-    X_i = [mat(x[inds[i]:inds[i+1]]) for (i, _) in enumerate(psds)]
+    X_i = [get_mat_from_lt_vec(x[inds[i]:inds[i+1]], n_i[i]) for (i, _) in enumerate(psds)]
 
     # apply change of basis on bilinear form (M = psds * blockdiag(x) * psds^T)
     Ms = []
     for (i, psd) in enumerate(psds)
-        M_i = vec(psd * X_i[i] * psd')
+        M_i = get_vec_from_lt_mat(psd * X_i[i] * psd')
         push!(Ms, M_i)
     end
 
@@ -248,4 +323,9 @@ function sos_to_qp(f, n, x)
     return cone_qp
 end
 
-export decompose
+export SOS_Symmetric_Group
+export wedderburn_decompose
+export get_constraint
+export get_mat_dim
+export get_mat_from_lt_vec
+export get_vec_from_lt_mat
